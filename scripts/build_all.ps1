2a33ae65816c0549cc34b19c09f39fd87d3e4c88
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:04:35 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,11 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "SMOD_Outbreak_0.45" `
+  -ModName "SMOD Outbreak 0.45" `
+  -ModFolder "smodoutbreak" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/smod-outbreak/smodoutbreak-0.45-Original.zip" `
+  -ModBaseFilesUrlHash "95d623105ec4621eff51cad5a1a7f6496eb3f95cb757901b7a2fa51428e8aa16" `
+  -ModReadmePath "readme.txt" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
